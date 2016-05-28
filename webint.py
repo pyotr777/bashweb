116c94dd0a5ce040c4266f121eae699eb7f80955
Peter Bryzgalov <peterbryz@yahoo.com>
2016-05-29 00:31:57 +0900

    Added restrictions on available commands.

--- webint.py
+++ webint.py
@@ -1,49 +1,75 @@
 #!/usr/bin/env python
 
-import bottle
+from bottle import Bottle, run, request
 import subprocess
 import re
+import urllib
 
-# Substitutions
-subst = ["","-"]
+webint = Bottle()
+
 # Permitted hosts
 accessList = ["localhost","127.0.0.1"]
+allowedCommands = ["git\s(.)*", "ls\s(.)*"]
+compiled = False
 
 # Check access origin
 def allowAccess():
-    remoteaddr = bottle.request.environ.get('REMOTE_ADDR')
-    forwarded = bottle.request.environ.get('HTTP_X_FORWARDED_FOR')
+    remoteaddr = request.environ.get('REMOTE_ADDR')
+    forwarded = request.environ.get('HTTP_X_FORWARDED_FOR')
 
     if (remoteaddr in accessList) or (forwarded in accessList):
         return True
     else:
         return False
 
+def allowCommand(command):
+    global compiled
+    compiled_dic = {}
+    print "Checking command "+ command
+    if not compiled:
+        print "Compiling command patterns"
+        for pattern_str in allowedCommands:
+            compiled_dic[pattern_str] = re.compile(pattern_str)
 
-@bottle.route('/')
-@bottle.route('/hello')
+        compiled = True
+    for pattern_str in compiled_dic:
+        pattern = compiled_dic[pattern_str]
+        if pattern.match(command) != None:
+            print "Command "+command+" matched pattern " + pattern_str
+            return True
+    print "No patterns matched. Command "+command+" not allowed."
+    return False
+
+
+@webint.route('/')
+@webint.route('/hello')
 def hello():
     return "Hello World!"
 
-@bottle.route('/exec/<esc_command>')
+@webint.route('/exec/<esc_command>')
 def exec_command(esc_command='pwd'):
     if allowAccess():
         pass
     else:
-        return "Access dinied"
+        return "Access denied."
 
-    command = esc_command.split("_")
+    command = urllib.unquote_plus(esc_command)
+
+    if allowCommand(command):
+        pass
+    else:
+        return "Command not allowed."
+
     try:
-        output = subprocess.check_output(command,stderr=subprocess.STDOUT)
-        #output = re.sub(subst[0],subst[1],output)
+        output = subprocess.check_output(command.split(),stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as ex:
         return "Error. cmd='"+ " ".join(ex.cmd)+ "' returncode="+ str(ex.returncode)
     else:        
         return "<html>\n<pre>\n"+output+"</pre>\n</html>"
 
-@bottle.route('/myhost')
+@webint.route('/myhost')
 def display_remote_host():
     return bottle.request.environ.get('REMOTE_ADDR')
 
-bottle.run(host='localhost', port=8080, debug=True)
+run(webint,host='localhost', port=8080, debug=True)
 
